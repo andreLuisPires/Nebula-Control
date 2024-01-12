692e4ad91b3b85a133f53edd07193465b4ce804b
André Luis <andrellui12345@gmail.com>
2024-01-12 08:28:59 -0400

    Pages and Authentication

---
+++ quasar-estoque/src/pages/EmailConfirmation.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page padding>
+    <p class="text-center q-pa-xs text-body1"><b>Obrigado por se Registrar!</b></p>
+    <p class="">
+      Por favor confirme o seu email para finalizar o registro <b>{{ $route.query.email }}</b>
+    </p>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PageEmailConfirmation'
+})
+</script>
--- quasar-estoque/src/pages/Register.vue
+++ quasar-estoque/src/pages/Register.vue
@@ -1,23 +1,11 @@
 <template>
   <q-page padding>
-    <q-form class="row justify-center q-pt-xl" @submit.prevent="">
-      <p class="col-12 text-h5 text-center">Registrar-se</p>
+    <q-form class="row justify-center q-pt-xl" @submit.prevent="handleRegister">
+      <p class="col-12 text-h5 text-center">Cadastrar</p>
       <div class="col-md-4 col-sm-6 col-xs-10 q-gutter-y-md">
-        <q-input 
-          label="Nome" 
-          outlined
-          v-model="form.name"
-        />
-        <q-input 
-          label="Email" 
-          outlined
-          v-model="form.email"
-        />
-        <q-input 
-          label="Senha"
-          outlined
-          v-model="form.password"
-        />
+        <q-input label="Nome" outlined v-model="form.name" />
+        <q-input label="Email" outlined v-model="form.email" />
+        <q-input label="Senha" outlined v-model="form.password" />
         <div class="full-width q-pt-md">
           <q-btn
             label="Registrar-se"
@@ -28,46 +16,54 @@
             type="submit"
           />
         </div>
-        
+        <div class="full-width">
+          <q-btn
+            label="Voltar"
+            color="primary"
+            class="full-width"
+            flat
+            to="/login"
+          />
+        </div>
       </div>
     </q-form>
   </q-page>
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref } from "vue";
 import useAuthUser from "src/composables/UseAuthUser";
 import { useRouter } from "vue-router";
 
 export default defineComponent({
-  name: 'RegisterPage',
+  name: "RegisterPage",
 
-  setup () {
-    const router = useRouter()
-    const { register } = useAuthUser()
+  setup() {
+    const router = useRouter();
+    const { register } = useAuthUser();
 
     const form = ref({
       name: '',
       email: '',
-      password: '' 
-    })
+      password: '',
+    });
 
     const handleRegister = async () => {
       try {
-        await register(form.value)
+        await register(form.value);
         router.push({
-          name: 'email-confirmation',
-          query: { email: form.value.email}
-        })
+          name: "email-confirmation",
+          query: { email: form.value.email }
+        });
       } catch (error) {
-        alert(error)
+        alert("Aconteceu algo inesperado: " + error.message);
       }
-    }
+    };
 
     return {
       form,
-      handleRegister
-    }
-  }
-})
+      handleRegister,
+    };
+  },
+});
 </script>
