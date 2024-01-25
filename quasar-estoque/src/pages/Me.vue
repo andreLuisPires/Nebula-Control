35dd82f164de35c9e2ca4de0726e983d84fe16f9
André Luis <andrellui12345@gmail.com>
2024-01-25 02:20:45 -0400

    Reusable components

--- quasar-estoque/src/pages/Me.vue
+++ quasar-estoque/src/pages/Me.vue
@@ -1,17 +1,37 @@
 <template>
-  <q-page class="flex flex-center">
-    <div v-if="user">
-      <p>Olá, <b>{{ user.user_metadata.name }}</b>.</p>
+  <q-page padding>
+    <div class="row q-col-gutter-md">
+      <div class="col-md-4 col-xs-12 col-sm-6">
+        <CardDashboard
+          table="category"
+          label="Categorias"
+          icon="mdi-shape-outline"
+
+        />
+      </div>
+      <div class="col-md-4 col-xs-12 col-sm-6">
+        <CardDashboard
+          table="product"
+          label="Produtos"
+          icon="mdi-archive"
+        />
+      </div>
     </div>
+    <!-- <div v-if="user">
+      <p>Olá, <b>{{ user.user_metadata.name }}</b>.</p>
+    </div> -->
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent, defineAsyncComponent } from 'vue'
 import useAuthUser from 'src/composables/UseAuthUser'
 
 export default defineComponent({
   name: 'PageMe',
+  components: {
+    CardDashboard: defineAsyncComponent(() => import('components/CardDashboard.vue'))
+  },
   setup () {
     const { user } = useAuthUser()
 
